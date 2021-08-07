d45ff00451e223dd67d425cd9450ef9aa24d4eab
TotoroDavid <davidtoro65@hotmail.com>
2021-08-07 06:30:39 -0500

    setting backend

---
+++ client/src/components/Posts/Post/Post.jsx
@@ -0,0 +1,14 @@
+import useStyles from './styles'
+
+const Post = () => {
+
+    const classes = useStyles()
+
+    return (
+        <div>
+            <h1>single post!!</h1>
+        </div>
+    )
+}
+
+export default Post
