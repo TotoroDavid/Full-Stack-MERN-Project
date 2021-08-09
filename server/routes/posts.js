ce0d6b0666276fe4c3c2b1a69af01adb0579ec32
TotoroDavid <davidtoro65@hotmail.com>
2021-08-09 12:11:41 -0500

    finishig the backend part

---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from 'express'
+//controllers
+import { getPosts, createPost } from '../controllers/posts.js'
+
+const router = express.Router()
+
+router.get('/', getPosts)
+router.post('/', createPost)
+
+export default router
